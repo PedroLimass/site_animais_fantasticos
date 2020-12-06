3bdb1622f25fb13deab028c4d56d51e832bc8d49
Pedro Lima <48936052+PedroLimass@users.noreply.github.com>
2020-12-05 21:35:28 -0300

    Add efeito de animação do scroll na page. Usando como referencia o top da pagina.

--- script.js
+++ script.js
@@ -68,3 +68,25 @@ function initScrollSuave(){
 }
 
 initScrollSuave();
+
+function initAnimaScroll(){
+  const sections = document.querySelectorAll('.js-scroll');
+  if(sections.length){
+    const windoRule = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section)=>{
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionView = (sectionTop - windoRule) < 0;
+        if(isSectionView){
+          section.classList.add('ativo');
+        }else section.classList.remove('ativo');
+      })
+    }
+  }
+
+  animaScroll(); //Para ativar pelo menos uma vez!
+  window.addEventListener('scroll', animaScroll);
+}
+
+initAnimaScroll();
