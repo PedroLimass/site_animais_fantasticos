badb1cafbcbf97be164c1485024e5070678d2b03
Pedro Lima <48936052+PedroLimass@users.noreply.github.com>
2020-12-11 20:43:42 -0300

    Criação e configuração do estilo di nidak

--- js/script.js
+++ js/script.js
@@ -2,10 +2,12 @@ import initScrollSuave from './modules/scroll-suave.js';
 import initAnimaScroll from './modules/animacao-scroll.js';
 import initAccordion from './modules/accordion-animation.js';
 import initTabNav from './modules/tabNav.js';
+import initModal from './modules/modal.js';
 
 
 initScrollSuave();
 initAnimaScroll();
 initAccordion();
 initTabNav();
+initModal();
 
