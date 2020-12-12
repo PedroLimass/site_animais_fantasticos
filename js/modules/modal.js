e00f6263f78b123afd8f925445face91fa5e0ba6
Pedro Lima <48936052+PedroLimass@users.noreply.github.com>
2020-12-12 19:45:48 -0300

    Add funcionalidade toggle ao modal de login

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,4 +1,22 @@
-export default function initModal(){
+export default function initModal() {
+  const botAbrir = document.querySelector('[data-modal="abrir"]');
+  const botFechar = document.querySelector('[data-modal="fechar"]');
+  const botContainer = document.querySelector('[data-modal="container"]');
 
+  if (botContainer && botFechar && botContainer) {
+    function toggleModal(event) {
+      event.preventDefault();
+      botContainer.classList.toggle("ativo");
+    }
+
+    function outModal(event) {
+      if (event.target === this) {
+        toggleModal(event);
+      }
+    }
+
+    botAbrir.addEventListener("click", toggleModal);
+    botFechar.addEventListener("click", toggleModal);
+    botContainer.addEventListener("click", outModal);
+  }
 }
-
