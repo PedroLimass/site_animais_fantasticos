859f554b4b4a84b98844581530fe0ae39a241705
Pedro Lima <48936052+PedroLimass@users.noreply.github.com>
2020-12-12 20:36:25 -0300

    Criando tooltio

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ import initAnimaScroll from './modules/animacao-scroll.js';
 import initAccordion from './modules/accordion-animation.js';
 import initTabNav from './modules/tabNav.js';
 import initModal from './modules/modal.js';
+import initToolTip from './modules/tooltip';
 
 
 initScrollSuave();
@@ -10,4 +11,4 @@ initAnimaScroll();
 initAccordion();
 initTabNav();
 initModal();
-
+initToolTip();
