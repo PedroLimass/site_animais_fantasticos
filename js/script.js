3dd475397a28233aa2d2a085315416098bbd7ac8
Pedro Lima <48936052+PedroLimass@users.noreply.github.com>
2020-12-15 22:12:26 -0300

    Add tooltip ao site

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ import initAnimaScroll from './modules/animacao-scroll.js';
 import initAccordion from './modules/accordion-animation.js';
 import initTabNav from './modules/tabNav.js';
 import initModal from './modules/modal.js';
-import initToolTip from './modules/tooltip';
+import initToolTip from './modules/tooltip.js';
 
 
 initScrollSuave();
