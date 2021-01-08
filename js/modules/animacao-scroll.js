c94e5ac9a2a9e65eb3a15cca9693378bda7d4586
Pedro Lima <48936052+PedroLimass@users.noreply.github.com>
2021-01-07 22:27:10 -0300

    Criação de animação para o campo de numeros

--- js/modules/animacao-scroll.js
+++ js/modules/animacao-scroll.js
@@ -1,20 +1,21 @@
-export default function initAnimaScroll(){
+export default function initAnimaScroll() {
   const sections = document.querySelectorAll('[data-anime="scroll"]');
-  if(sections.length){
+  if (sections.length) {
     const windoRule = window.innerHeight * 0.6;
 
     function animaScroll() {
-      sections.forEach((section)=>{
+      sections.forEach((section) => {
         const sectionTop = section.getBoundingClientRect().top;
-        const isSectionView = (sectionTop - windoRule) < 0;
-        if(isSectionView){
-          section.classList.add('ativo');
-        }else
-            section.classList.remove('ativo');
-      })
+        const isSectionView = sectionTop - windoRule < 0;
+        if (isSectionView) {
+          section.classList.add("ativo");
+        } else if (section.classList.add("ativo")) {
+          section.classList.remove("ativo");
+        }
+      });
     }
 
     animaScroll(); //Para ativar pelo menos uma vez!
-    window.addEventListener('scroll', animaScroll);
-  }     
+    window.addEventListener("scroll", animaScroll);
+  }
 }
