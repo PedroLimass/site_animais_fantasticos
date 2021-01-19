e8054dd2c0d2ffd9d73b9e0d35c82e20f09fdff4
Pedro Lima <48936052+PedroLimass@users.noreply.github.com>
2021-01-19 14:10:57 -0300

    Add arquivos para pratica de fetch e apis

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ import initModal from "./modules/modal.js";
 import initToolTip from "./modules/tooltip.js";
 import initDropDownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
-import initAnimaNumeros from "./modules/anima-numeros.js";
 import initFuncionamento from "./modules/funcionamento.js";
+import initFetchAnimais from "./modules/fetch-animais.js";
 
 initScrollSuave();
 initAnimaScroll();
@@ -17,5 +17,6 @@ initModal();
 initToolTip();
 initDropDownMenu();
 initMenuMobile();
-initAnimaNumeros();
+
 initFuncionamento();
+initFetchAnimais();
